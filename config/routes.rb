280af28fb5528722b1d327ccc9fc46905b146603
ohkubo kohei <kuboon@trick-with.net>
2020-08-01 23:17:35 +0900

    rails g controller home index host --skip-helper --skip-assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/host'
   get 'api/send_sound'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
